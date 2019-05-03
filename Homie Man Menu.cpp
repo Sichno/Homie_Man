3fc8b5fac187dd27c01bac085f62ea42741dadff
Sichno <39745752+Sichno@users.noreply.github.com>
2019-05-03 10:55:31 -0700

    Update Homie Man Menu.cpp

--- Homie Man Menu.cpp
+++ Homie Man Menu.cpp
@@ -5,17 +5,24 @@ using namespace std;
  
 int main()
 {
-    char name[50];
+    char p_name[50];
     cout << "..." << endl;
+    _getch();
     cout << "...." << endl;
+    _getch();
     cout << "....." << endl;
+    _getch();
     cout << "Hey foo, you're finally awake!" << endl;
     cout << "You were trying to get some tamales right?" << endl;
+    _getch();
     cout << "Damn you Hood rats! The hood was fine until you came along." << endl;
-    cout << "40's n ho's all day in the hood, until they came along" << endl;
+    cout << "40's n ho's all day in the hood, until they came along..." << endl;
+    _getch();
     cout << "Ay Foo, whats your name anyways?" << endl;
-    cin.getline(name, 50);
-    cout << "You better book it fast, " << name << ". The Hood Rats are bound to come back." << endl;
+    cin.getline(p_name, 50);
+    cout << "You better book it fast, " << p_name << ". The Hood Rats are bound to come back." << endl;
+    _getch();
+    cout << "There, grab that shank there! Hurry before they come back!" << endl;
     cout << "\n----------------------Press any key to continue----------------------" << endl;
     _getch();
     return 0;
