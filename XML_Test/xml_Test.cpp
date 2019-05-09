dcca1ae8056b242c9ad8447802e425856aeb7093
Sichno <maciasdiegoam@gmai.com>
2019-05-09 16:03:00 -0700

    Xml_to_data then Data_to_dialogue

--- XML_Test/xml_Test.cpp
+++ XML_Test/xml_Test.cpp
@@ -3,25 +3,24 @@
 #include <stdlib.h>
 #include <chrono>
 #include <thread>
-//#include <Windows.h>
-
 using namespace std;
 
+//Data_To_Dialogue will ask this function (Xml_Dialogue)
+//to parse xml for dialogue, it will then hand it back to to Data_Dialogue
+//Which in turn hands it off to the main.cpp
+
+
 int main()
-
 {
-
-string hello = string("This is a test");
-
-int x=0;
-
-while ( hello[x] != '\0')
-
-	{
-		cout << hello[x];
-		std::this_thread::sleep_for (std::chrono::nanoseconds(99999999));
-	x++;
-	};
-	
-	return 0;
+// Read attribute value
+pugi::xml_attribute attr;
+if (attr = selectedNode.attribute("intVal")) // attribute really exists
+{
+	// Read value as string
+	std::cout << "read as string: intVal=" << attr.value() << std::endl;
+     // Read value as int
+	int intVal = attr.as_int();
+	std::cout << "read as int   : intVal=" << intVal << std::endl;
+     // for other types use as_double, as_bool, as_uint, ...
+ }
 }
